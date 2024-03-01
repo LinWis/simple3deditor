2d4a91978fda6db19e07f2ffd7d8db9649fe2ba0
Turbin Viktor <turbinvr1@gmail.com>
2024-03-01 21:27:27 +0300

    state

--- src/components/MainControl.vue
+++ src/components/MainControl.vue
@@ -1,36 +1,80 @@
 <script setup lang="ts">
-import { ref } from 'vue';
+  import { ref } from 'vue';
 
   const showSidebar = ref(false);
+
+  function addMesh() {
+    
+  }
+
 </script>
 
 <template>
-  <button 
-    :class="{'fixed right-0 text-white': !showSidebar}"
-    @click="showSidebar = !showSidebar"
-  >
-    <img
-      width="40px"
-      src="../assets/left-arrow.svg"
+  <Transition name="slide-btn">
+    <button 
+      v-if="!showSidebar"
+      class="fixed right-0 text-white"
+      @click="showSidebar = !showSidebar"
     >
-  </button>  
-  <div
-    class="w-2/12 min-h-full bg-zinc-950 overflow-hidden text-white fixed right-0 items-center flex-col justify-between py-10"
-    :class="{'flex': showSidebar, 'hidden': !showSidebar}"
-  >
-    <div class="control__header w-full text-left px-5">
-      <button @click="showSidebar =!showSidebar">
-        <img 
-          width="35px"
-          src="../assets/left-arrow-line.svg"
-        >
-      </button>
+      <img
+        width="40px"
+        src="../assets/left-arrow.svg"
+      >
+    </button>  
+  </Transition>
+
+  <Transition name="slide-fade">
+    <div
+      v-if="showSidebar"
+      class="w-2/12 min-h-full bg-zinc-950 overflow-hidden text-white fixed right-0 items-center flex flex-col justify-between py-10"
+    >
+      <div class="control__header w-full text-left px-5">
+        <button @click="showSidebar =!showSidebar">
+          <img 
+            width="35px"
+            src="../assets/left-arrow-line.svg"
+          >
+        </button>
+        <div class="add-mesh flex justify-center">
+          <button 
+            class="bg-zinc-900 p-2 rounded-md hover:bg-zinc-800"
+            @click="addMesh"
+          >
+            Add mesh
+          </button>
+        </div>
+      </div>
+      <div class="control__content">
+        content
+      </div>
+      <div class="control__footer">
+        footer
+      </div>
     </div>
-    <div class="control__content">
-      content
-    </div>
-  </div>
+  </Transition>
 </template>
 
-<style scoped lang="">
+<style scoped>
+  .slide-fade-enter-active {
+    transition: all 0.6s ease-out;
+  }
+
+  .slide-fade-leave-active {
+    transition: all 0.6s cubic-bezier(1, 0.5, 0.8, 1);
+  }
+
+  .slide-btn-enter-from, .slide-btn-leave-active {
+    opacity: 100%;
+  }
+
+  .slide-btn-enter-active, .slide-btn-leave-from {
+    transition: all 0.5s linear;
+    opacity: 0;
+  }
+
+
+  .slide-fade-enter-from, .slide-fade-leave-to {
+    transform: translateX(20px);
+    opacity: 0;
+  }
 </style>
